8f961568948d45e47d4f0d2ab42ad015926f43c0
Justin Bassett <jbassett271@gmail.com>
2021-01-12 21:57:40 -0800

    Support Windows with winflexbison

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile
+from conans.tools import os_info
+
+class NocternConan(ConanFile):
+    requires = (
+        'boost/1.74.0',
+        'fmt/7.1.2',
+        'spdlog/1.8.1',
+        'frozen/1.0.0',
+    )
+    build_requires = (
+        'catch2/2.13.3',
+    )
+
+    def build_requirements(self):
+        if os_info.is_windows:
+            self.build_requires('winflexbison/2.5.22')
+        else:
+            self.build_requires('flex/2.6.4')
