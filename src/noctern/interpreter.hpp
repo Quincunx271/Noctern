4aaa30891c12e9a7a2f52d8e51c33f1a7697be7c
Justin Bassett <jbassett@google.com>
2025-02-27 01:00:27 -0800

    Hack an interpreter

---
+++ src/noctern/interpreter.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <optional>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "noctern/symbol_table.hpp"
+#include "noctern/tokenize.hpp"
+
+namespace noctern {
+    class interpreter {
+    public:
+        struct frame {
+            std::unordered_map<std::string_view, double> locals;
+            std::vector<double> expr_stack;
+        };
+
+        explicit interpreter(symbol_table table)
+            : table_(std::move(table)) {
+        }
+
+        double eval_fn(const tokens& source, token from) const;
+
+    private:
+        double eval_block(const tokens& source, frame& frame, tokens::const_iterator& pos) const;
+
+        double eval_expr(const tokens& source, frame& frame, tokens::const_iterator& pos) const;
+
+        symbol_table table_;
+    };
+}
