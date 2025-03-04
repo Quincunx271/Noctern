6546bc314de827cf88af798988dd1e2f001ed69a
Justin Bassett <jbassett@google.com>
2025-03-04 01:33:58 -0800

    Use CMakePresets.json (#7)
    * Set up CMakePresets.json
    
    * CI: Set up CMakePresets.json
    
    * CI: Fix yaml syntax error
    
    * CI: Fix a couple typos
    
    * CI: Teach the test reporter about the build path
    
    * CI: See about collapsing the -Werror display

---
+++ cmake/toolchains/asan.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_CXX_FLAGS_INIT -fsanitize=address)
+set(CMAKE_EXE_FLAGS_INIT -fsanitize=address)
+set(CMAKE_SHARED_LINKER_FLAGS_INIT -fsanitize=address)
+set(CMAKE_MODULE_LINKER_FLAGS_INIT -fsanitize=address)
