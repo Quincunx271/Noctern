786e1a3a536e13f4fc89fd6da15b8c4e86446b39
Justin Bassett <jbassett@google.com>
2025-03-07 00:34:21 -0800

    Sketch out a frontend "IR"
    Unsure if "IR" is the correct term. The idea is to model computation as
    a register machine.
    
    I think the type system can similarly be built as a register machine,
    with instructions to (algebraically) compose types, e.g. by doing loose
    sum and products, with other operations to ascribe field names / proper
    names or formalize as an actual type vs. just a "temporary" type.

--- src/noctern/intern_table.hpp
+++ src/noctern/intern_table.hpp
@@ -1,25 +1,75 @@
 #pragma once
 
+#include <array>
+#include <concepts>
 #include <cstdint>
+#include <limits>
+#include <span>
 #include <string_view>
 #include <unordered_map>
 #include <vector>
 
+#include "noctern/meta.hpp"
+
 namespace noctern {
     template <typename T, typename InternIndexType>
+        requires std::unsigned_integral<InternIndexType>
     class intern_table;
 
     namespace intern_table_internal {
         template <typename T, typename InternIndexType>
+            requires std::unsigned_integral<InternIndexType>
         class interned {
             friend intern_table<T, InternIndexType>;
             friend std::hash<interned>;
 
         public:
-            friend bool operator==(interned, interned) = default;
+            constexpr interned(const interned&) = default;
+            constexpr interned(interned&&) noexcept = default;
+
+            friend constexpr bool operator==(interned, interned) = default;
+
+            template <typename Word>
+                requires std::integral<Word>
+            static constexpr auto encoded_size(type_t<Word>) {
+                constexpr auto Div = sizeof(InternIndexType) / sizeof(Word);
+                constexpr auto Rem = sizeof(InternIndexType) % sizeof(Word);
+                constexpr auto DivRoundingUp = Div + (Rem != 0 ? 1 : 0);
+                return DivRoundingUp;
+            }
+
+            template <typename Word>
+                requires std::integral<Word>
+            constexpr auto encode_as(type_t<Word>) const {
+                constexpr auto MaxWord = std::numeric_limits<Word>::max();
+                constexpr auto WordBits = std::numeric_limits<Word>::digits;
+
+                std::array<Word, encoded_size(type<Word>)> result {};
+                InternIndexType data = index_;
+                for (size_t index = 0; index < result.size(); ++index) {
+                    result[index] = data & MaxWord;
+                    // TODO: disable -Wmissing-field-initializers
+                    data >>= WordBits;
+                }
+                return result;
+            }
+
+            template <typename Word>
+                requires std::integral<Word>
+            static constexpr interned decode_from(std::span<const Word> data) {
+                constexpr auto WordBits = std::numeric_limits<Word>::digits;
+
+                InternIndexType result = 0;
+                for (size_t indexp1 = encoded_size(type<Word>); indexp1 > 0; --indexp1) {
+                    result <<= WordBits;
+                    result |= data[indexp1 - 1];
+                }
+
+                return interned {result};
+            }
 
         private:
-            constexpr interned(InternIndexType index)
+            explicit constexpr interned(InternIndexType index)
                 : index_(index) {
             }
 
@@ -28,6 +78,7 @@ namespace noctern {
     }
 
     template <typename T, typename InternIndexType = uint32_t>
+        requires std::unsigned_integral<InternIndexType>
     class intern_table {
     public:
         using interned = intern_table_internal::interned<T, InternIndexType>;
